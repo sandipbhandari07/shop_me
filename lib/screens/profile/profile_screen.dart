f20fe44c6e21af3bef00935dfd471d73d86ca675
sandipbhandari07 <bhandarisandip882@gmail.com>
2024-08-21 17:54:32 +0545

    initial push

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -13,7 +13,7 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Profile not happeninig')),
+      appBar: AppBar(title: Text('Profile dadanot happeninig')),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
